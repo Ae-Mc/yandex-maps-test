0872a9c2abd8f20345b9f9625337f8c655409086
Ae-Mc <ae_mc@mail.ru>
2022-02-17 15:17:35 +0300

    build(deps): add dependencies, update gradle and add yandex maps support to MainActivity

--- android/app/src/main/kotlin/com/example/yandex_maps_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yandex_maps_test/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.yandex_maps_test
 
+import androidx.annotation.NonNull
+import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("3bc979ab-cda3-4289-a67c-6d8563cc51d1")
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
